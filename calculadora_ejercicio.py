0b640396ad65622c522d36a90cf17cffd7ea0181
Bruno Salas <brunosalash@gmail.com>
2019-12-11 15:35:09 -0500

    Ejercicio con calculadora

--- calculadora_ejercicio.py
+++ calculadora_ejercicio.py
@@ -1,15 +1,12 @@
 print("Bienvenidos a la Calculadora Basica disenada por Bruno Salas")
-operacion = input("Que operacion quieres realizar? (Suma / Resta / Multiplicacion / Division ) :").upper
+operacion = input("Que operacion quieres realizar? (SUMA / RESTA / MULTIPLICACION / DIVISION ) :")
 a = float(input("Escoge el primer numero: "))
-b = float(input("Escoge el primer numero: "))
-resultado = 0
-
-if operacion == "Suma":
-    resultado = a+b
-elif operacion == "Resta":
-    resultado = a-b
-elif operacion == "Multipliacion":
-    resultado = a*b
-elif operacion == "Division":
-    resultado = a/b
-print("El resultado de tu operacion es {}".format(resultado))
+b = float(input("Escoge el segundo numero: "))
+if operacion == "SUMA":
+    print("el resultado es: ", a + b)
+elif operacion == "RESTA":
+    print("el resultado es: ", a - b)
+elif operacion == "MULTIPLICACION":
+    print("el resultado es: ", a * b)
+elif operacion == "DIVISION":
+    print("el resultado es: ", a / b)
