81a41f51a6cb432bed5f5682ac338ae271088575
Bruno Salas <brunosalash@gmail.com>
2019-12-11 22:22:05 -0500

    Ejercicio con calculadora

---
+++ convertidor_farenheit_celsius.py
@@ -0,0 +1,4 @@
+print("Bienvenidos al convertidor de grados Farenheit a Celsius y viceversa")
+farenheit = int(input("Cuantos grados tienes : "))
+celsius = (farenheit - 32 ) * 5/9
+print("La conversion de " + str(farenheit) + ' grados farenheit son ' + str(celsius) + ' grados celsius.')
